346c0adc0b568a661f10dd7fefd2242f1bdde3f0
omerhagag2511 <omerhagag2511@gmail.com>
2026-03-31 21:25:48 +0300

    2nd commit

--- app/src/main/java/com/example/project13/MainActivity.java
+++ app/src/main/java/com/example/project13/MainActivity.java
@@ -1,24 +1,70 @@
 package com.example.project13;
-
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch swDN;
+    RadioButton b1;
+    RadioButton b2;
+    RadioButton b3;
+    RadioButton b4;
+    ConstraintLayout layout;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        swDN = findViewById(R.id.swDN);
+        layout = findViewById(R.id.layout);
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+        b3 = findViewById(R.id.b3);
+        b4 = findViewById(R.id.b4);
+
+    }
+    public void go(View view) {
+        if (swDN.isChecked()){
+            if (b1.isChecked()){
+                layout.setBackgroundColor(Color.RED);
+            }
+            if (b2.isChecked()){
+                layout.setBackgroundColor(Color.RED);
+
+            }
+            if (b3.isChecked()){
+                layout.setBackgroundColor(Color.RED);
+
+            }
+            if (b4.isChecked()){
+                layout.setBackgroundColor(Color.RED);
+
+            }
+        }
+        else{
+            if (b1.isChecked()){
+                layout.setBackgroundColor(Color.RED);
+
+            }
+            if (b2.isChecked()){
+                layout.setBackgroundColor(Color.PINK);
+
+            }
+            if (b3.isChecked()){
+                layout.setBackgroundColor(Color.RED);
+
+            }
+            if (b4.isChecked()){
+                layout.setBackgroundColor(Color.RED);
+
+            }
+        }
     }
 }
